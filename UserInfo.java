b15d05d4522ede40e8d75503b12a57c40603ce20
Sara Ahmed <saraalsuliemani@gmail.com>
2025-07-22 11:53:06 +0400

    display all info

--- UserInfo.java
+++ UserInfo.java
@@ -4,19 +4,28 @@ public class UserInfo {
     private string email;
     private boolean isActive;
 
-    public UserInfo( string name, int age, string email, boolean isActive){
+    public UserInfo(string name, int age, string email, boolean isActive) {
         this.name = name;
         this.age = age;
         this.email = email;
         this.isActive = isActive;
     }
-    public String getName() {return name;}
 
-    public int getAge() {return age;}
+    public String getName() {
+        return name;
+    }
 
-    public String getEmail() {return email;}
+    public int getAge() {
+        return age;
+    }
 
-    public boolean getIsActive() {return isActive;}
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
 
     public void displayInfo() {
         System.out.println("User Information:");
@@ -26,6 +35,20 @@ public class UserInfo {
         System.out.println("Active   : " + (isActive ? "Yes" : "No"));
     }
 
+    public static void main(String[] args) {
+        UserInfo user1 = new UserInfo("Sara", 21, "sarara@example.com", true);
+        UserInfo user2 = new UserInfo("alaa", 22, "lulu@example.com", false);
+        UserInfo user3 = new UserInfo("fatma", 25, "fatma@example.com", true);
+        user1.displayInfo();
+        user2.displayInfo();
+        user3.displayInfo();
 
-
+        user1.setName("maryam");
+        user1.setAge(33);
+        user1.setIsActive(false);
+        System.out.println("Updated name of user1:" + " " + user1.getName());
+        System.out.println("Updated age of user1:" + " " + user1.getAge());
+        System.out.println("Is user1 active?" + " " + user1.getIsActive());
+        user1.displayInfo();
+    }
 }
