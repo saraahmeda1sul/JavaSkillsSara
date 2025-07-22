35598eaad7d6b463e92d21cde811736f5b8d5dae
Sara Ahmed <saraalsuliemani@gmail.com>
2025-07-22 11:42:31 +0400

    constructor

--- UserInfo.java
+++ UserInfo.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class UserInfo {
+    private string name;
+    private int age;
+    private string email;
+    private boolean isActive;
+
+    public UserInfo( string name, int age, string email, boolean isActive){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.isActive = isActive;
+    }
+    public String getname(){
+        return name;
+    }
+
+
+
 }
