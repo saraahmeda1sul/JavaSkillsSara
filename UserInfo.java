640d39f720442da319ca19f466842804552e0f5b
Sara Ahmed <saraalsuliemani@gmail.com>
2025-07-22 11:46:58 +0400

    getter and setter

--- UserInfo.java
+++ UserInfo.java
@@ -10,8 +10,20 @@ public class UserInfo {
         this.email = email;
         this.isActive = isActive;
     }
-    public String getname(){
-        return name;
+    public String getName() {return name;}
+
+    public int getAge() {return age;}
+
+    public String getEmail() {return email;}
+
+    public boolean getIsActive() {return isActive;}
+
+    public void displayInfo() {
+        System.out.println("User Information:");
+        System.out.println("Name     : " + name);
+        System.out.println("Age      : " + age);
+        System.out.println("Email    : " + email);
+        System.out.println("Active   : " + (isActive ? "Yes" : "No"));
     }
 
 
